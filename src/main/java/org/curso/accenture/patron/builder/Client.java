c02b7e40704fc772fd7d29d4e6502f9f3dcafee9
jose78 <jlmmmorales@gmail.com>
2017-11-06 18:03:34 +0100

    Más ficheros2

--- src/main/java/org/curso/accenture/patron/builder/Client.java
+++ src/main/java/org/curso/accenture/patron/builder/Client.java
@@ -4,5 +4,14 @@ public class Client {
   private Algorithm algorithm1;
   private Algorithm algorithm2;
   
-  
+  public Client() {
+    algorithm1 = new Algorithm("QuickSort",10, 10, 20, "Sum");
+    
+    algorithm2 = new AlgorithmBuilder()
+        .setName("MergeSort")
+        .setMaximunNumberOfEvaluations(10)
+        .setMemoryRequirement(10)
+        .setOperator("Sum")
+        .build();
+  }
 }
